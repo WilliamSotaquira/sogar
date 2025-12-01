09a7c432f4e37d425c0d3f3aa24ed698f2d0c7ef
WilliamSotaquira <william.sotaquira@gmail.com>
2025-11-30 22:05:32 -0500

    chore: round logo and display Sogar label

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,4 @@
 <div class="flex items-center gap-2">
-    <img src="{{ asset('icon_xs.png') }}" alt="Sogar" class="h-8 w-8 object-contain lg:hidden">
-    <img src="{{ asset('icon_lg.png') }}" alt="Sogar Finanzas" class="hidden h-8 object-contain lg:block">
-    <span class="sr-only">Sogar Finanzas</span>
+    <img src="{{ asset('icon_xs.png') }}" alt="Sogar" class="h-9 w-9 rounded-lg object-cover">
+    <span class="text-sm font-semibold text-gray-900 dark:text-white">Sogar</span>
 </div>
