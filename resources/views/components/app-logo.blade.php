4b75d39a317105166c29f80626b618b6bcae5d86
WilliamSotaquira <william.sotaquira@gmail.com>
2025-11-30 22:01:11 -0500

    chore: simplify logo display to avoid overlapping text

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,8 +1,5 @@
 <div class="flex items-center gap-2">
     <img src="{{ asset('icon_xs.png') }}" alt="Sogar" class="h-8 w-8 object-contain lg:hidden">
     <img src="{{ asset('icon_lg.png') }}" alt="Sogar Finanzas" class="hidden h-8 object-contain lg:block">
-    <div class="grid text-start text-sm leading-tight">
-        <span class="font-semibold">Sogar</span>
-        <span class="text-xs text-gray-700 dark:text-gray-200">Finanzas</span>
-    </div>
+    <span class="sr-only">Sogar Finanzas</span>
 </div>
