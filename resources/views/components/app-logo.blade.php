760735fdf204ad96e4485733d7d5b734a4f1860c
WilliamSotaquira <william.sotaquira@gmail.com>
2025-11-30 21:50:16 -0500

    chore: point app logo components to sogar assets

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,4 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+<div class="flex items-center gap-2">
+    <x-app-logo-icon class="h-8 w-8" />
+    <img src="{{ asset('images/sogar-wordmark.png') }}" alt="Sogar" class="h-6 object-contain">
 </div>
